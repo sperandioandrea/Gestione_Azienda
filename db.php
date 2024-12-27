97ade4f105649e40abd72bc87d967da0fad832a9
andrea <sperandio.andrea.studente@itispaleocapa.it>
2024-12-27 16:39:39 +0100

    db.php: Collegamento con il database. Utilizzo del comando PDO

--- db.php
+++ db.php
@@ -1,3 +1,16 @@
 <?php
 
+$host = 'localhost'; 
+$dbname = 'dbaziendale';
+$username = 'root'; 
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Errore di connessione: " . $e->getMessage());
+}
+
 ?>
+
